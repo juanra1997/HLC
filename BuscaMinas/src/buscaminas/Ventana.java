34f31bbb9eb0abb142e629f39e3e71ab3eb4246c
juanra1997 <juanrainmortal22@yahoo.com>
2018-10-30 13:22:25 +0100

    +

--- BuscaMinas/src/buscaminas/Ventana.java
+++ BuscaMinas/src/buscaminas/Ventana.java
@@ -6,11 +6,24 @@
 package buscaminas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -19,6 +32,7 @@ import javax.swing.JPanel;
 public class Ventana extends JFrame{
     
     miPanel panel;
+    miPanelLateral lateral;
     
     public Ventana(){
         
@@ -32,9 +46,24 @@ public class Ventana extends JFrame{
         for(int i=0; i<64; i++){
             add(new JButton());
         }*/
+        
+        //----------------------------------------------------------------------
+        //Descomentar esto
         panel=new miPanel();
-        add(panel, BorderLayout.CENTER  );
+        panel.setBorder(BorderFactory.createCompoundBorder());
+        add(panel, BorderLayout.CENTER);
         //add(new JButton(), BorderLayout.NORTH);
+        /*setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+        //----------------------------------------------------------------------
+        
+        lateral=new miPanelLateral();
+        lateral.setPreferredSize(new Dimension(150, 40));
+        //lateral.setBorder(BorderFactory.createCompoundBorder());
+        add(lateral, BorderLayout.LINE_END);
+        
+        
         setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,40 +72,133 @@ public class Ventana extends JFrame{
 
 class miPanel extends JPanel {
     
+    GridLayout gl=new GridLayout(8, 8);
+    JButton[][] botones=new JButton[8][8];
+    
     public miPanel(){
         
         iniciaComponentes();
     }
     public void iniciaComponentes(){
         
-        setLayout(new GridLayout(8, 8));
+        setLayout(gl);
+        setBackground(Color.WHITE);
+        gl.setHgap(5);
+        gl.setVgap(5);
         //JOptionPane.showMessageDialog(null, "hola");
-        for(int i=0; i<64; i++){
+        /*for(int i=0; i<64; i++){
             add(new JButton());
+        }*/
+        
+        //----------------------------------------------------------------------
+        //Descomentar esto
+        for(int i=0; i<botones.length; i++){
+            for(int j=0; j<botones[i].length; j++){
+                //System.out.println(i+","+j);
+                botones[i][j]= new JButton("M");
+                botones[i][j].setBackground(Color.CYAN);
+            }
+        }
+        for(int i=0; i<botones.length; i++){
+            for(int j=0; j<botones[i].length; j++){
+                add(botones[i][j]);
+            }
         }
         //add(new JButton("Hola"));
         
         setVisible(true);
+        //----------------------------------------------------------------------
     }
 
 }
 
 class miPanelLateral extends JPanel {
     
+    JButton iniciar, pausar, salir;
+    JTextField banderas, tiempo;
+    
     public miPanelLateral(){
         
         iniciaComponentes();
     }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+		
+        
+        super.paintComponent(g);
+            
+        File fichero=new File("C:\\Users\\Juanra\\Desktop\\sintitulo.png");
+        File fichero2=new File("C:\\Users\\Juanra\\Desktop\\34116.png");
+        
+        try {
+            Image miImagen=ImageIO.read(fichero);
+            g.drawImage(miImagen, 50, 10, null);
+        } catch (IOException ex) {
+            Logger.getLogger(miPanelLateral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            Image miImagen=ImageIO.read(fichero2);
+            g.drawImage(miImagen, 50, 100, null);
+        } catch (IOException ex) {
+            Logger.getLogger(miPanelLateral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       
+    }
+    
     public void iniciaComponentes(){
         
-        //setLayout(new GridLayout(8, 8));
-        //JOptionPane.showMessageDialog(null, "hola");
-        for(int i=0; i<64; i++){
-            add(new JButton());
-        }
-        //add(new JButton("Hola"));
+        setLayout(null);
         
-        setVisible(true);
+        setBackground(Color.WHITE);
+        setBorder(BorderFactory.createCompoundBorder());
+        
+        iniciar=new JButton("INICIAR");
+        
+        iniciar.setBounds(25, 220, 100, 25);
+        iniciar.setForeground(Color.BLUE);
+        add(iniciar);
+        
+        pausar=new JButton("PAUSAR");
+        
+        pausar.setBounds(25, 260, 100, 25);
+        pausar.setForeground(new Color(68, 121, 65));
+        pausar.setEnabled(false);
+        add(pausar);
+        
+        salir=new JButton("SALIR");
+        
+        salir.setBounds(25, 300, 100, 25);
+        salir.setForeground(Color.RED);
+        salir.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                System.exit(0);
+            }
+        });
+        add(salir);
+        
+        banderas=new JTextField();
+        
+        banderas.setBackground(Color.BLACK);
+        banderas.setForeground(Color.GREEN);
+        banderas.setBounds(25, 60, 100, 25);
+        banderas.setHorizontalAlignment(JTextField.CENTER);
+        banderas.setText("00/10");
+        add(banderas);
+        
+        tiempo=new JTextField();
+        
+        tiempo.setBackground(Color.BLACK);
+        tiempo.setForeground(Color.GREEN);
+        tiempo.setBounds(25, 160, 100, 25);
+        tiempo.setHorizontalAlignment(JTextField.CENTER);
+        tiempo.setText("00:00");
+        add(tiempo);
     }
 
 }
